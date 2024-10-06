b34782b1b35f823791603d6b558d61f0dfb91e0b
Alexis <fkahope@Mac.local>
2024-10-06 17:38:01 +0200

    Initialization

---
+++ src/main/java/com/alexis/ApplicationSpringTh/ApplicationSpringThApplication.java
@@ -0,0 +1,13 @@
+package com.alexis.ApplicationSpringTh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSpringThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSpringThApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexis/ApplicationSpringTh/ApplicationSpringThApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexis.ApplicationSpringTh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSpringThApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
