e469e9910318ecc18de8490963db366bb6799143
Alexis <fkahope@Mac.local>
2024-10-07 18:28:35 +0200

    Template new Product

--- src/main/java/com/alexis/ApplicationSpringTh/controller/ProductController.java
+++ src/main/java/com/alexis/ApplicationSpringTh/controller/ProductController.java
@@ -4,9 +4,8 @@ package com.alexis.ApplicationSpringTh.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.alexis.ApplicationSpringTh.model.Product;
 import com.alexis.ApplicationSpringTh.repository.ProductRepository;
-
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -20,5 +19,12 @@ public class ProductController {
         model.addAttribute("products", productRepository.findAll());
         return "index";
         // Index correspondra au fichier html sur lequel le repo sera retourné
+    }
+
+    @GetMapping("/product/new")
+    public String newProduct(Model model){
+        // On retourne un nouveau produit dans le model
+        model.addAttribute("product", new Product());
+        return "newProduct";
     }
 }
